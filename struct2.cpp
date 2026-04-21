7c557ae07f54a1335d7145eda5a7834e74044422
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-21 10:48:26 +0700

    //i/o dan cin

--- struct2.cpp
+++ struct2.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 using namespace std;
 
-struct DetailAlamat { //membuat struct detail alamat
-    string desa;
-    string kota;
+struct DetailAlamat {
+    string desa; //village
+    string kota;//city
 };
 
-struct mahasiswa { //membuat struktur mahasiswa
+struct mahasiswa {
     string NIM;
     string nama;
     DetailAlamat alamat;
     int umur;
 };
 
-int main(){  //membuat int main untuk  i/o
+int main(){  //i/o dan cin
     mahasiswa mhs;
     cout << "nomor Mahasiswa : ";
     cin >> mhs.NIM;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
-    cout << "Alamat Mahasiswa : ";
-    cin >> mhs.alamat;
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t nama desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota: ";
+    cin >> mhs.alamat.kota;
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
 
     cout << endl;
     cout << " \nNIM : " << mhs.NIM;
-    cout << "Nama : " << mhs.nama;
-    cout << "\nAlamat : ";
-    cout << "\nDesa : "<< mhs.alamat.desa;
-    cout << "\nKota :" << mhs.alamat.kota;
-    cout << "Umur : " << mhs.umur;  
+    cout << " \nNama : " << mhs.nama;
+    cout << " \nAlamat : " ;
+    cout << " \nDesa : " << mhs.alamat.desa;
+    cout << " \nKota : " << mhs.alamat.kota;
+    cout << " \nUmur : " << mhs.umur;  
 }
