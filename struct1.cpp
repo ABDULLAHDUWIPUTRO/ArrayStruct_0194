1283d7c64a37cbb9f9a7c30823411377309652c9
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-21 09:41:50 +0700

    Menambahkan struktur data struct mahasiswa dan fungsi input

---
+++ struct1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main(){
+    mahasiswa mhs;
+    cout << "nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << " \nNIM : " << mhs.NIM;
+    cout << "Nama : " << mhs.nama;
+    cout << "Alamat : " << mhs.alamat;
+    cout << "Umur : " << mhs.umur;  
+}
