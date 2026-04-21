229137df7727db5e39210dd53571eba35a7360fc
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2026-04-21 10:05:49 +0700

    //membuat struktur mahasiswa

---
+++ struct3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main(){
+    mahasiswa mhs;
+    cout << "nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << " \nNIM : " << mhs.NIM;
+    cout << "Nama : " << mhs.nama;
+    cout << "\nAlamat : ";
+    cout << "\nDesa : "<< mhs.alamat.desa;
+    cout << "\nKota :" << mhs.alamat.kota;
+    cout << "Umur : " << mhs.umur;  
+}
